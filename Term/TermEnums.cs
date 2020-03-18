76d09fecdfbaf57498a39941df891448a349ae93
Фролов <nikita.grigorevich@gmail.com>
2020-03-18 11:45:53 +0500

    init

--- Term/TermEnums.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Term
-{
-    public enum Align { Left, Center, Right }
-
-    public enum FloatDirection { Horisontal, Vertical }
-
-    public enum Decoration {None,  Framed, DoubleFramed}
-}
